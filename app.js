1b53e4654663af4ee017848972546a34d3f55971
Alec Hancock <alec.m.hancock@gmail.com>
2023-04-14 11:19:43 -0500

    Adjusted functions to return as intended.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function printOdds(count){
 
     for(var i = 0; i < count; i++){
         if(i % 2 != 0){
-            console.log(i)
+            return (i)
         }
         else{
             continue;
@@ -24,13 +24,13 @@ let nullParam = "One or more parameters not provided, please try again."
 let aboveSixteen = `Congrats, ${userName}, you can drive!`
 let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
 if (userName == null || age == null){
-    console.log(nullParam)
+    return nullParam
 }
 else if (age > 16){
-    console.log(aboveSixteen)
+    return belowSixteen
 }
 else{
-    console.log(belowSixteen)
+    return aboveSixteen
 }
 }
 
@@ -39,25 +39,25 @@ else{
 console.log("EXERCISE 2:\n==========\n");
 function quadrantCheck(x, y){
     if (x , y == 0){
-        console.log("The point lies on the center.")
+        return ("The point lies on the center.")
     }
     else if( x = 0 && y != 0){
-        console.log("The point lies on the x axis.")
+        return ("The point lies on the x axis.")
     }
     else if( y = 0 && x != 0){
-        console.log("The point lies on the y axis.")
+        return ("The point lies on the y axis.")
     }
     else if(x > 0 && y > 0){
-        console.log("The point lies in quadrant 1.")
+        return ("The point lies in quadrant 1.")
     }
     else if( x > 0 && y < 0){
-        console.log("The point lies in quadrant 4.")
+        return ("The point lies in quadrant 4.")
     }
     else if(x < 0 && y > 0){
-        console.log("The point lies in quadrant 2.")
+        return ("The point lies in quadrant 2.")
     }
     else if(x < 0 && y < 0){
-        console.log("The point lies in quadrant 3.")
+        return ("The point lies in quadrant 3.")
     }
 }
 
@@ -69,18 +69,18 @@ console.log("EXERCISE 2:\n==========\n");
 
 function triangleType(side1, side2, side3){
     if (side1 = side2 = side3){
-        console.log("This is an equilateral triangle.")
+        return ("This is an equilateral triangle.")
     }
     if (side1 + side2 == side3 || side1+ side3 == side2 || side3 + side2 == side1){
         if(side1 != side2 != side3){
-            console.log("This is a scalene triangle.")
+            return ("This is a scalene triangle.")
         }
         if(side1 == side2 || side1 == side3 || side2 == side3){
-            console.log("This is an isoceles triangle.")
+            return ("This is an isoceles triangle.")
         }
     }
     else{
-        console.log("This is not a triangle.")
+        return ("This is not a triangle.")
     }
 
 }
@@ -99,21 +99,21 @@ function dataPlan(planLimit, day, usage){
     console.log(`${day} days utilized, ${dayTrack} days remaining. 
     Average daily use: ${avgUse} GB/day.`)
     if (avgUse > actualUse){
-        console.log(`You are under your average daily use, 
+        return (`You are under your average daily use, 
         using ${actualUse} GB/day. At the current rate, 
         you will use ${expectedUse} GB of your ${planLimit} GB plan. 
         You can use up to ${remaining} 
         GB/day and remain within your limits.`)
     }
     if(avgUse < actualUse){
-        console.log(`You are exceeding your average daily use, 
+        return (`You are exceeding your average daily use, 
         using ${actualUse} GB/day. At the current rate, 
         you will use ${expectedUse} GB of your ${planLimit} GB plan. 
         You can use up to ${remaining} 
         GB/day and remain within your limits.`)
     }
     if(avgUse = actualUse){
-        console.log(`You are within your average daily use, 
+        return (`You are within your average daily use, 
         using ${actualUse} GB/day. At the current rate, 
         you will use ${expectedUse} GB of your ${planLimit} GB plan. 
         You can use up to ${remaining} 
